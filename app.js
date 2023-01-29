21a915227f14c52eea97bf2c90c1e32a64928dd8
Sualeh Siddiqui <sualehmsiddiqui@gmail.com>
2023-01-29 21:03:47 +0500

    adding js assignment 2 chap 4
    adding js assignment 2 chap 4

---
+++ app.js
@@ -0,0 +1,19 @@
+// Chap #04
+
+// Question#1
+// var name , email , password;
+
+// Question#2
+// Leagal Var
+// var name , $pass , _message , num1 , name_mail;
+
+// Ill-leagal Var
+// var 1num , name email , message-greet , @the , #name
+
+// Question#3
+
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("Variable names can only contain letter, numbers, $ and _.For example $my_1stVariable");
+document.write("<br />Variables must begin with a letter, $ or _. For example $name, _name or name");
+document.write("<br /> Variable names are case sensitive.");
+document.write("<br /> Variable names should not be JS keywords.");
